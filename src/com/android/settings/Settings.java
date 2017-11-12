af451b1a16ff493ffe3fec96f8b0ec7c8deff483
mj1687 <alessandro.pasini12@gmail.com>
2017-11-12 10:49:23 +0100

    Settings: Build DNA inside settings
    This commits includes all the fixes and patches from the old nougat
    branch
    
    higly helped from the commit of @mnemonyc for the discovery settings
    
    Signed-off-by: mj1687 <alessandro.pasini12@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -191,4 +191,7 @@ public class Settings extends SettingsActivity {
     public static class UserAndAccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
 
+    //DNA
+    public static class Splash extends SettingsActivity { /* empty */ }
+
 }
